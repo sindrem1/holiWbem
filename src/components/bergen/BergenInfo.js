89048dacd1b6111a515832c9d2d0e43075f75f0a
sindrem1 <54024047+sindrem1@users.noreply.github.com>
2021-02-12 20:21:08 +0100

    changes

--- src/components/bergen/BergenInfo.js
+++ src/components/bergen/BergenInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Mountains from '../assets/mountains.jpg'
 import Fish from '../assets/fish.jpg'
+import Shop from '../assets/shop.jpg'
 
 function BergenInfo() {
     return (
@@ -27,9 +28,9 @@ function BergenInfo() {
             <div className="bergen__columntwo">
                 <img src={Fish} alt="Fishing" className="bergen__imgtwo" />
             </div>
-                        <h2 className="bergen__header">Mountains</h2>
+                        <h2 className="bergen__header">Shopping</h2>
             <div className="bergen__column">
-                <img src={Mountains} alt="Mountains" className="bergen__img" />
+                <img src={Shop} alt="Shopping" className="bergen__img" />
             </div>
             <div className="bergen__column">
                 <p className="bergen__textright">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -37,17 +38,6 @@ function BergenInfo() {
                 Pellentesque tincidunt, ante et molestie ullamcorper, erat lorem
                 scelerisque ex, ac mollis eros metus vel diam. Donec venenatis,
                 libero vel tincidunt posuere, urna ante venenatis nisl.</p>
-            </div>
-            <h2 className="bergen__header">Fishing</h2>
-            <div className="bergen__columntwo">
-                <p className="bergen__textleft">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Sed dictum ullamcorper dolor, consectetur aliquam ipsum pharetra ac.
-                Pellentesque tincidunt, ante et molestie ullamcorper, erat lorem
-                scelerisque ex, ac mollis eros metus vel diam. Donec venenatis,
-                libero vel tincidunt posuere, urna ante venenatis nisl.</p>
-            </div>
-            <div className="bergen__columntwo">
-                <img src={Fish} alt="Fishing" className="bergen__imgtwo" />
             </div>
         </div>
 
